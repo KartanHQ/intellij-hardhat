37b5ef76225228b99366b0cb78d5c45d4437afdf
Milad Nekofar <milad@nekofar.com>
2022-02-01 20:34:48 +0330

    chore(deps-dev): change intellij type and version

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,8 @@ dependencies {
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version.set("2021.3.1")
+    version.set("2021.1")
+    type.set("IU")
 }
 tasks {
     patchPluginXml {
