70581e2fb3be97a9fa959eb239abf062763878cf
Milad Nekofar <milad@nekofar.com>
2022-02-01 16:07:47 +0330

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("org.jetbrains.intellij") version "1.3.1"
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "com.nekofar.milad"
+version = "1.0.0-alpha.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version.set("2021.3.2")
+}
+tasks {
+    patchPluginXml {
+        changeNotes.set("""
+            Add change notes here.<br>
+            <em>most HTML tags may be used</em>        """.trimIndent())
+    }
+}
