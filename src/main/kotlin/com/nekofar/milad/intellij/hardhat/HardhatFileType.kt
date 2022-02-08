7e43f2be1cf05625a99e3bbc34966839b5c7f097
Milad Nekofar <milad@nekofar.com>
2022-02-08 13:16:44 +0330

    refactor: customize file type icon for hardhat configs

--- src/main/kotlin/com/nekofar/milad/intellij/hardhat/HardhatFileType.kt
+++ src/main/kotlin/com/nekofar/milad/intellij/hardhat/HardhatFileType.kt
@@ -1,6 +1,5 @@
 package com.nekofar.milad.intellij.hardhat
 
-import com.intellij.icons.AllIcons
 import com.intellij.lang.javascript.JavascriptLanguage
 import com.intellij.openapi.fileTypes.LanguageFileType
 import javax.swing.Icon
@@ -9,5 +8,5 @@ object HardhatFileType: LanguageFileType(JavascriptLanguage.INSTANCE) {
     override fun getName(): String = HardhatBundle.message("hardhat.file.type.name")
     override fun getDescription(): String = HardhatBundle.message("hardhat.file.type.description")
     override fun getDefaultExtension(): String = "js"
-    override fun getIcon(): Icon = AllIcons.FileTypes.Config
+    override fun getIcon(): Icon = HardhatIcons.FileType
 }
--- src/main/kotlin/com/nekofar/milad/intellij/hardhat/HardhatIcons.kt
+++ src/main/kotlin/com/nekofar/milad/intellij/hardhat/HardhatIcons.kt
@@ -4,5 +4,8 @@ import com.intellij.openapi.util.IconLoader
 
 object HardhatIcons {
     @JvmField
+    val FileType = IconLoader.getIcon("/icons/hardhat.svg", javaClass)
+
+    @JvmField
     val ProjectGenerator = IconLoader.getIcon("/icons/hardhat.png", javaClass)
 }
