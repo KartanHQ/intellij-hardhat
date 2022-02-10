05bca08886aea0a477fba28b5a1c242817c7239a
Milad Nekofar <milad@nekofar.com>
2022-02-10 22:05:41 +0330

    build: enable java code support on build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,19 @@
 import org.jetbrains.changelog.markdownToHTML
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
+    // Java support
+    id("java")
+    // Kotlin support
+    id("org.jetbrains.kotlin.jvm") version "1.6.10"
     // Gradle IntelliJ Plugin
     id("org.jetbrains.intellij") version "1.3.1"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "1.3.1"
     // Gradle Qodana Plugin
     id("org.jetbrains.qodana") version "0.1.13"
-    // Kotlin support
-    kotlin("jvm") version "1.6.10"
 }
 
 group = properties("pluginGroup")
@@ -18,10 +21,6 @@ version = properties("pluginVersion")
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    implementation(kotlin("stdlib"))
 }
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
@@ -56,7 +55,7 @@ tasks {
             sourceCompatibility = it
             targetCompatibility = it
         }
-        withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        withType<KotlinCompile> {
             kotlinOptions.jvmTarget = it
         }
     }
