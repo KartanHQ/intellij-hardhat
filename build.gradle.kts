dfa352e67fce22bd7389e3beebe794064f0244e1
arne-fuchs <arnefuchs@protonmail.com>
2023-12-27 17:31:05 +0000

    Fixed missing dependency for testing for IDE 2023.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,8 @@ repositories {
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
 //    implementation(libs.annotations)
+    implementation("com.fasterxml.jackson.core:jackson-core:2.13.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.2")
 }
 
 // Set the JVM language level used to build the project. Use Java 11 for 2020.3+, and Java 17 for 2022.2+.
